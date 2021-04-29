cad221067900ca77a2dc7c54dc12159cca7a083c
Richard <richard.yang.tong@gmail.com>
2021-04-29 18:02:23 -0400

    Added simple if statemnt.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -3,6 +3,9 @@ package academy.learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("It is not an alien!");
     }
 }
