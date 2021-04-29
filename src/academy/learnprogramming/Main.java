2daa434e4a533e49212c6233506ea46dd53153a0
Richard <richard.yang.tong@gmail.com>
2021-04-29 18:06:52 -0400

    Added another way to writing the if statement.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         boolean isAlien = true;
-        if (isAlien == false);
+        if (isAlien)
             System.out.println("It is not an alien!");
     }
 }
