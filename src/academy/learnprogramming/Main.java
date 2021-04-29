14d007e9d00094833e7e5105fc9d1923f44d6b49
Richard <richard.yang.tong@gmail.com>
2021-04-29 18:11:24 -0400

    Added another if statement.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -9,5 +9,10 @@ public class Main {
             System.out.println("It is not an alien!");
             System.out.println("And I am scared of aliens");
         }
+
+        int topScore = 100;
+        if (topScore > 80) {
+            System.out.println("You got the high score!");
+        }
     }
 }
