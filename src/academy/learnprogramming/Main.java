6605943d71eb8d01a734332d3015ed17b49cf3ad
Richard <richard.yang.tong@gmail.com>
2021-04-29 18:05:27 -0400

    Added a incorrect case of white space.

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean isAlien = false;
-        if (isAlien == false)
+        boolean isAlien = true;
+        if (isAlien == false);
             System.out.println("It is not an alien!");
     }
 }
